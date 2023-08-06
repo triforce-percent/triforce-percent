7ba9743aacaa5d44e284b00dfd7b5ca751f73a45
Sauraen <sauraen@gmail.com>
2023-08-05 22:31:04 -0700

    Fixed Amogus lighting issue

--- actor/Amogus/Amogus.c
+++ actor/Amogus/Amogus.c
@@ -3,7 +3,8 @@
 #include "AmogusObjIdleAnim.h"
 
 #define DEP_OBJECT_NUM 0x84
-#define SHADOW_SIZE 40.0f
+#define SHADOW_SIZE 25.0f
+#define ACTOR_SCALE 0.018f
 
 static ColliderCylinderInitType1 sCylinderInit = {
     { COLTYPE_HIT0, AT_NONE, AC_NONE, OC1_ON | OC1_TYPE_PLAYER, COLSHAPE_CYLINDER, },
@@ -25,9 +26,9 @@ static void init(Entity *en, GlobalContext *globalCtx) {
 	ActorShape_Init(&en->actor.shape, 0.0f, ActorShadow_DrawCircle, SHADOW_SIZE);
 	Collider_InitCylinder(globalCtx, &en->collider);
     Collider_SetCylinderType1(globalCtx, &en->collider, &en->actor, &sCylinderInit);
-	SkelAnime_InitFlex(globalCtx, &en->skelAnime, &AmogusObj, &AmogusObjIdleAnim, 
+	SkelAnime_Init(globalCtx, &en->skelAnime, &AmogusObj, &AmogusObjIdleAnim, 
 		en->jointTable, en->morphTable, AMOGUSOBJ_NUM_LIMBS);
-	Actor_SetScale(&en->actor, 0.018f);
+	Actor_SetScale(&en->actor, ACTOR_SCALE);
 }
 
 static void destroy(Entity *en, GlobalContext *globalCtx) {
@@ -40,12 +41,6 @@ static void updateCommon(Entity *en, GlobalContext *globalCtx) {
     CollisionCheck_SetOC(globalCtx, &globalCtx->colChkCtx, &en->collider.base);
 	Actor_UpdateBgCheckInfo(globalCtx, &en->actor, 75.0f, 30.0f, 30.0f, 5);
 	SkelAnime_Update(&en->skelAnime);
-	en->actor.focus.pos.x = en->actor.world.pos.x;
-    en->actor.focus.pos.y = en->actor.world.pos.y + 40.0f;
-    en->actor.focus.pos.z = en->actor.world.pos.z;
-    en->actor.focus.rot.x = en->actor.world.rot.x;
-    en->actor.focus.rot.y = en->actor.world.rot.y;
-    en->actor.focus.rot.z = en->actor.world.rot.z;
 }
 
 static void updateTalking(Entity *en, GlobalContext *globalCtx);
@@ -53,6 +48,7 @@ static void updateTalking(Entity *en, GlobalContext *globalCtx);
 static void updateWait(Entity *en, GlobalContext *globalCtx) {
 	updateCommon(en, globalCtx);
 	if(Actor_IsTalking(&en->actor, globalCtx)){
+        Audio_SetBGM(0x71);
 		Animation_Change(&en->skelAnime, &AmogusObjIdleAnim,
 			1.0f, 0.0f, 2,
 			ANIMMODE_LOOP, 10.0f);
@@ -72,10 +68,31 @@ static void updateTalking(Entity *en, GlobalContext *globalCtx) {
 	}
 }
 
+s32 Amogus_OverrideLimbDraw(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3f* pos, Vec3s* rot, void* thisx) {
+	return false;
+}
+
+void Amogus_PostLimbDraw(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3s* rot, void* thisx) {
+	Entity *en = (Entity*)thisx;
+
+    if (limbIndex == AMOGUSOBJ_ROOT_ROT_LIMB) {
+        Vec3f HeadBoneOriginToHeadCenter = { 0.0f, 60.0f / ACTOR_SCALE, 0.0f };
+        Vec3f HeadCenterOut;
+
+        Matrix_MultVec3f(&HeadBoneOriginToHeadCenter, &HeadCenterOut);
+        en->actor.focus.pos.x = HeadCenterOut.x;
+        en->actor.focus.pos.y = HeadCenterOut.y;
+        en->actor.focus.pos.z = HeadCenterOut.z;
+        en->actor.focus.rot.x = en->actor.world.rot.x;
+        en->actor.focus.rot.y = en->actor.world.rot.y;
+        en->actor.focus.rot.z = en->actor.world.rot.z;
+    }
+}
+
 static void draw(Entity *en, GlobalContext *globalCtx) {
 	func_80093D18(globalCtx->state.gfxCtx);
-	SkelAnime_DrawFlexOpa(globalCtx, en->skelAnime.skeleton, en->skelAnime.jointTable,
-		en->skelAnime.dListCount, NULL, NULL, en);
+	SkelAnime_DrawOpa(globalCtx, en->skelAnime.skeleton, en->skelAnime.jointTable,
+		Amogus_OverrideLimbDraw, Amogus_PostLimbDraw, en);
 	func_80093D18(globalCtx->state.gfxCtx);
 }
 
